76de8fb5a64aa099cfa54182c9528de4ddb25775
sandip <sandipsanjel@users.noreply.github.com>
2023-08-31 22:03:11 +0545

    created migration and seeder

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {//this seeder is used to seed the user data in database like user loging in into the system
+        DB::table('users')->insert([
+            'name'=>'tom holland',
+                 'email'=>'tom@gmail.com',
+                 'password'=>Hash::make('123')
+        ]);
+    }
+}
