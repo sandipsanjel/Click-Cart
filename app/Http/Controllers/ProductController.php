56ad95a99e61a30e7d8f0b01e2c0c82232955133
sandip <sandipsanjel@users.noreply.github.com>
2023-09-09 18:33:52 +0545

    logout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\cart;
-
-use Session;
+use Illuminate\Support\Facades\session;
 
 
 class ProductController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-
+use Illuminate\Support\Facades\session;
 
 
 /*
@@ -19,6 +19,11 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/logout', function () {
+    Session::forget('user'); //to forget the users session from browser
+    return redirect('login');
 });
 
 Route::post ("/login",[UserController::class,'login']);
