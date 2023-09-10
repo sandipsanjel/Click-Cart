ec3535db4f822170ef2d541126a0f724bfc47988
sandip <sandipsanjel@users.noreply.github.com>
2023-09-10 18:32:56 +0545

    product seed

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,34 +16,28 @@ class ProductSeeder extends Seeder
     {
         //
         DB::table('products')->insert([
-            [
-                'name'=>'Yatri Bike',
-                "price"=>"500000",
-                "description"=>"Yatri bike price in Nepal starts at Rs. 5,65,000 for the most affordable model, P1, and goes up to Rs. 19,49,000 for the most expensive model",
-                "category"=>"bike",
-                "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrOBh7Y9NILDSYsjto1KJvKEyZXX7uT0ZAgd7gdg1LDQ&s"
-            ],
             // [
-            //     'name'=>'Panasonic Tv',
-            //     "price"=>"400",
-            //     "description"=>"A smart tv with much more feature",
-            //     "category"=>"tv",
-            //     "gallery"=>"https://i.gadgets360cdn.com/products/televisions/large/1548154685_832_panasonic_32-inch-lcd-full-hd-tv-th-l32u20.jpg"
+            //     'name'=>'Yatri Bike',
+            //     "price"=>"500000",
+            //     "description"=>"Yatri bike price in Nepal starts at Rs. 5,65,000 for the most affordable model, P1, and goes up to Rs. 19,49,000 for the most expensive model",
+            //     "category"=>"bike",
+            //     "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrOBh7Y9NILDSYsjto1KJvKEyZXX7uT0ZAgd7gdg1LDQ&s"
             // ],
             // [
-            //     'name'=>'Soni Tv',
+            //     'name'=>'iphone',
+            //     "price"=>"1000",
+            //     "description"=>"Powerful. Beautiful. Durable. 5G connectivity. Check out the new iPhone 14 Pro, iPhone 14 Pro Max, iPhone 14, iPhone 14 Plus and iPhone SE.",
+            //     "category"=>"Mobile",
+            //     "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqQSYJ-J6ytO53HBYiMveLk3QkVgaTfloqupoWlG8Mhw&s"
+            // ],
+            // [
+            //     'name'=>'Tesla',
             //     "price"=>"500",
-            //     "description"=>"A tv with much more feature",
-            //     "category"=>"tv",
-            //     "gallery"=>"https://4.imimg.com/data4/PM/KH/MY-34794816/lcd-500x500.png"
+            //     "description"=>"Tesla is accelerating the world's transition to sustainable energy with electric cars",
+            //     "category"=>"Car",
+            //     "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw0ug3p_8DmHXyMZltsffGvN5GOmVgCWwSvw"
             // ],
-            // [
-            //     'name'=>'LG fridge',
-            //     "price"=>"200",
-            //     "description"=>"A fridge with much more feature",
-            //     "category"=>"fridge",
-            //     "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFx-2-wTOcfr5at01ojZWduXEm5cZ-sRYPJA&usqp=CAU"
-            //  ]
+
         ]);
     }
 }
